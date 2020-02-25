b35f6958897461f238da378c6b785dcaefbf88c6
Sandeep Yadav <sandeep.yadav@siemens.com>
2020-02-25 19:32:20 +0530

    package update

--- becs/src/main/java/com/siemens/becs/variables/Variables.java
+++ becs/src/main/java/com/siemens/becs/variables/Variables.java
@@ -13,6 +13,14 @@ public class Variables {
 		return variables;
 	}
 
+	public void addVariable(Variable var) {
+		getVariables().add(var);
+	}
+
+	public void addVariable(String name, String value) {
+		addVariable(new Variable(name, value));
+	}
+
 	public static void updateValue(String varName, String value) {
 		variables.forEach(var -> {
 			if (var.getName().equals(varName))
